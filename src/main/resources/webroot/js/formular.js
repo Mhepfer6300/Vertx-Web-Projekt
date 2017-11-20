5ecc1adad6fdb42b01fd1e7184bcfed9fd839d0d
m.hepfer <m.hepfer@IR1-PC-15.musterschule.schule.paedml>
2017-11-20 17:23:05 +0100

    Passwort Eingabe hinzugefügt

--- src/main/resources/webroot/js/formular.js
+++ src/main/resources/webroot/js/formular.js
@@ -3,10 +3,11 @@ $(document).ready(function () {
         $.ajax({url:"../anfrage", data:
                 {
                     typ: "namenKnopf",
-                    name: $("#eingabeName").val()
+                    name: $("#eingabeName").val(),
+                    passwort: $("#passwort").val()
                 },
                 success: function (data) {
-                    $("body").append("<div>Daten: " + data.text+"<div>");
+                    $("body").append("<div>" + data.text+"<br>"+ data.passwort+"<div>");
                 }
             });
     });
